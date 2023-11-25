759002388b30a5bc4d0b269f7435ae669f447fd6
Nicholas Choong <nicholascwc1997@gmail.com>
2023-11-25 15:15:03 +0800

    change link

--- server/main.py
+++ server/main.py
@@ -10,9 +10,9 @@ app = FastAPI()
 
 origins = [
     "http://localhost",
-    "http://localhost:3000",
+    "http://localhost:3003",
     "http://127.0.0.1/",
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
 ]
 
 app.add_middleware(
